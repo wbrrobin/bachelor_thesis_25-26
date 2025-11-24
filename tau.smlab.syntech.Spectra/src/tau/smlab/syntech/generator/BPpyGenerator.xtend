2ec67e3e393383b3cfab45e33625eca62a518821
wbrrobin <weber_ro@gmx.de>
2025-11-25 00:23:42 +0100

    add bool, int and enum var translation

--- tau.smlab.syntech.Spectra/src/tau/smlab/syntech/generator/BPpyGenerator.xtend
+++ tau.smlab.syntech.Spectra/src/tau/smlab/syntech/generator/BPpyGenerator.xtend
@@ -1,10 +1,15 @@
 package tau.smlab.syntech.generator
 
+import org.eclipse.emf.common.util.EList
 import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.xtext.generator.AbstractGenerator
 import org.eclipse.xtext.generator.IFileSystemAccess2
 import org.eclipse.xtext.generator.IGeneratorContext
 import tau.smlab.syntech.spectra.Model
+import tau.smlab.syntech.spectra.Subrange
+import tau.smlab.syntech.spectra.TypeConstant
+import tau.smlab.syntech.spectra.Var
+import tau.smlab.syntech.spectra.VarType
 
 class BPpyGenerator extends AbstractGenerator {
 	
@@ -18,6 +23,34 @@ class BPpyGenerator extends AbstractGenerator {
 	}
 	
 	def compile(Model model) '''
-		print(«model.name»)
+		from bppy import *
+		
+		«FOR variable : model.elements.filter(typeof(Var))»
+		«variable.^var.type.compile(variable.^var.name)»
+		«ENDFOR»
+	'''
+	
+	def compile(VarType type, String name) {
+		switch type {
+			case type.name == "boolean" : name + " = Bool('" + name + "')"
+			case type.subr !== null : addInt(type.subr, name)
+			case type.const !== null : addEnum(type.const, name)
+			//case type.type !== null : // TODO
+			// TODO: array option
+		}
+	}
+	
+	def addInt(Subrange subr, String name) '''
+		«name» = Int('«name»')
+		
+		@thread
+		def «name»Bounds():
+			yield sync(block=Or(«name» < «subr.from.value», «name» > «subr.to.value»))
+	'''
+	
+	def addEnum(EList<TypeConstant> constants, String name) '''
+		«name.toFirstUpper», («constants.map[it.name.toFirstLower].join(", ")») \
+			= EnumSort('«name.toFirstUpper»', («constants.map["'" + it.name + "'"].join(", ")»))
+		«name» = Const('«name»', «name.toFirstUpper»)
 	'''
 }
--- tau.smlab.syntech.Spectra/xtend-gen/tau/smlab/syntech/generator/BPpyGenerator.java
+++ tau.smlab.syntech.Spectra/xtend-gen/tau/smlab/syntech/generator/BPpyGenerator.java
@@ -1,13 +1,24 @@
 package tau.smlab.syntech.generator;
 
+import com.google.common.base.Objects;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Iterators;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.generator.AbstractGenerator;
 import org.eclipse.xtext.generator.IFileSystemAccess2;
 import org.eclipse.xtext.generator.IGeneratorContext;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.eclipse.xtext.xbase.lib.IteratorExtensions;
+import org.eclipse.xtext.xbase.lib.ListExtensions;
+import org.eclipse.xtext.xbase.lib.StringExtensions;
 import tau.smlab.syntech.spectra.Model;
+import tau.smlab.syntech.spectra.Subrange;
+import tau.smlab.syntech.spectra.TypeConstant;
+import tau.smlab.syntech.spectra.Var;
+import tau.smlab.syntech.spectra.VarType;
 
 @SuppressWarnings("all")
 public class BPpyGenerator extends AbstractGenerator {
@@ -24,9 +35,110 @@ public class BPpyGenerator extends AbstractGenerator {
 
   public CharSequence compile(final Model model) {
     StringConcatenation _builder = new StringConcatenation();
-    _builder.append("print(");
-    String _name = model.getName();
-    _builder.append(_name);
+    _builder.append("from bppy import *");
+    _builder.newLine();
+    _builder.newLine();
+    {
+      Iterable<Var> _filter = Iterables.<Var>filter(model.getElements(), Var.class);
+      for(final Var variable : _filter) {
+        CharSequence _compile = this.compile(variable.getVar().getType(), variable.getVar().getName());
+        _builder.append(_compile);
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    return _builder;
+  }
+
+  public CharSequence compile(final VarType type, final String name) {
+    CharSequence _switchResult = null;
+    boolean _matched = false;
+    String _name = type.getName();
+    boolean _equals = Objects.equal(_name, "boolean");
+    if (_equals) {
+      _matched=true;
+      _switchResult = (((name + " = Bool(\'") + name) + "\')");
+    }
+    if (!_matched) {
+      Subrange _subr = type.getSubr();
+      boolean _tripleNotEquals = (_subr != null);
+      if (_tripleNotEquals) {
+        _matched=true;
+        _switchResult = this.addInt(type.getSubr(), name);
+      }
+    }
+    if (!_matched) {
+      EList<TypeConstant> _const = type.getConst();
+      boolean _tripleNotEquals_1 = (_const != null);
+      if (_tripleNotEquals_1) {
+        _matched=true;
+        _switchResult = this.addEnum(type.getConst(), name);
+      }
+    }
+    return _switchResult;
+  }
+
+  public CharSequence addInt(final Subrange subr, final String name) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append(name);
+    _builder.append(" = Int(\'");
+    _builder.append(name);
+    _builder.append("\')");
+    _builder.newLineIfNotEmpty();
+    _builder.newLine();
+    _builder.append("@thread");
+    _builder.newLine();
+    _builder.append("def ");
+    _builder.append(name);
+    _builder.append("Bounds():");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t");
+    _builder.append("yield sync(block=Or(");
+    _builder.append(name, "\t");
+    _builder.append(" < ");
+    int _value = subr.getFrom().getValue();
+    _builder.append(_value, "\t");
+    _builder.append(", ");
+    _builder.append(name, "\t");
+    _builder.append(" > ");
+    int _value_1 = subr.getTo().getValue();
+    _builder.append(_value_1, "\t");
+    _builder.append("))");
+    _builder.newLineIfNotEmpty();
+    return _builder;
+  }
+
+  public CharSequence addEnum(final EList<TypeConstant> constants, final String name) {
+    StringConcatenation _builder = new StringConcatenation();
+    String _firstUpper = StringExtensions.toFirstUpper(name);
+    _builder.append(_firstUpper);
+    _builder.append(", (");
+    final Function1<TypeConstant, String> _function = (TypeConstant it) -> {
+      return StringExtensions.toFirstLower(it.getName());
+    };
+    String _join = IterableExtensions.join(ListExtensions.<TypeConstant, String>map(constants, _function), ", ");
+    _builder.append(_join);
+    _builder.append(") \\");
+    _builder.newLineIfNotEmpty();
+    _builder.append("\t");
+    _builder.append("= EnumSort(\'");
+    String _firstUpper_1 = StringExtensions.toFirstUpper(name);
+    _builder.append(_firstUpper_1, "\t");
+    _builder.append("\', (");
+    final Function1<TypeConstant, String> _function_1 = (TypeConstant it) -> {
+      String _name = it.getName();
+      String _plus = ("\'" + _name);
+      return (_plus + "\'");
+    };
+    String _join_1 = IterableExtensions.join(ListExtensions.<TypeConstant, String>map(constants, _function_1), ", ");
+    _builder.append(_join_1, "\t");
+    _builder.append("))");
+    _builder.newLineIfNotEmpty();
+    _builder.append(name);
+    _builder.append(" = Const(\'");
+    _builder.append(name);
+    _builder.append("\', ");
+    String _firstUpper_2 = StringExtensions.toFirstUpper(name);
+    _builder.append(_firstUpper_2);
     _builder.append(")");
     _builder.newLineIfNotEmpty();
     return _builder;
